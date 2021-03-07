dae24eb79c5ceec6652ed5ba97bfc4123c0af628
Ari Torkilsson Johannesen <69643316+Aritj@users.noreply.github.com>
2021-03-07 16:22:57 +0000

    Update app.py

--- projekt/python/app.py
+++ projekt/python/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Refresh tp-link IPs
 def refreshIPs():
-	os.system("../bash-scripts/get_hs100_ip_fast.sh")
+	os.system("../bash-scripts/get_hs100_ip.sh")
 
 # Refresh server stats
 def refresh_stats():
