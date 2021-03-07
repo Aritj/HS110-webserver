d9d606f2153b24b1df2e5591c3bcd83b6bfbdc02
Ari Torkilsson Johannesen <69643316+Aritj@users.noreply.github.com>
2021-03-07 18:43:26 +0000

    Update ext-cpu-memory-swap.sh

--- projekt/bash-scripts/ext-cpu-memory-swap.sh
+++ projekt/bash-scripts/ext-cpu-memory-swap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #!/bin/sh
 
-ssh -o ConnectTimeout=1 serverArnie "cat serverArnie.txt" >> data/server.txt'
-ssh -o ConnectTimeout=1 serverAlex "cat serverAlex.txt" >> data/server.txt'
+ssh -o ConnectTimeout=1 serverArnie "cat serverArnie.txt" >> data/server.txt
+ssh -o ConnectTimeout=1 serverAlex "cat serverAlex.txt" >> data/server.txt
