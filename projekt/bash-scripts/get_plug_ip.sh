32f5525a1559fa118779689746a3234877695328
Ari Torkilsson Johannesen <69643316+Aritj@users.noreply.github.com>
2021-03-08 08:38:46 +0000

    Update get_plug_ip.sh

--- projekt/bash-scripts/get_plug_ip.sh
+++ projekt/bash-scripts/get_plug_ip.sh
@@ -16,4 +16,5 @@ fi
 # Aggresively scan LAN network for port TCP/9999
 # Grep for devices with port TCP/9999 open
 # Grep for IP addresses of devices with port TCP/9999 open
-nmap -Pn -p 9999 $network_info | grep -B4 "open" | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' > ../python/data/ip.txt
+echo "$network_info" > ../python/data/ip.txt
+nmap -Pn -p 9999 $network_info | grep -B4 "open" | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' >> ../python/data/ip.txt
