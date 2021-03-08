2b111c369a638a320dffa146d772aab8a6328bbc
Ari Torkilsson Johannesen <69643316+Aritj@users.noreply.github.com>
2021-03-08 08:28:57 +0000

    Update get_plug_ip.sh

--- projekt/bash-scripts/get_plug_ip.sh
+++ projekt/bash-scripts/get_plug_ip.sh
@@ -5,8 +5,13 @@
 # Look into the 'config' file for subnet specification, save to variable 'network_info', whic will be used in the NMAP scan.
 network_info=$(cat ../config | grep subnet | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/2[0-9]')
 
-# Get LAN information and save to variable 'network_info', this variable will be used in the NMAP command.
-# network_info=$(ip -o -f inet addr show | grep 'eth0' | awk '/scope global/ {print $4}')
+if [[ -z "$network_config" ]]; then
+  # If network config is not OK, then get LAN information on 'eth0' and save to variable 'network_info', this subnet will be used in the NMAP command.
+  network_info=$(ip -o -f inet addr show | grep 'eth0' | awk '/scope global/ {print $4}')
+else
+  # If network config is OK, then use that subnet for NMAP command.
+  network_info=$network_config
+fi
 
 # Aggresively scan LAN network for port TCP/9999
 # Grep for devices with port TCP/9999 open
