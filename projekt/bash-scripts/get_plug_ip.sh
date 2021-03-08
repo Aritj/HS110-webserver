613eb83cad8ac9e812a28c682e4af3bf39beb53f
Ari Torkilsson Johannesen <69643316+Aritj@users.noreply.github.com>
2021-03-08 10:17:24 +0000

    Update get_plug_ip.sh

--- projekt/bash-scripts/get_plug_ip.sh
+++ projekt/bash-scripts/get_plug_ip.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #!/bin/sh
 
-# Get network information (IP address and subnet mask in CIDR notation).
-# Look into the 'config' file for subnet specification, save to variable 'network_info', whic will be used in the NMAP scan.
+
+# Check 'config' file for subnet specification, save to variable 'network_info'
 network_config=$(cat ../config | grep subnet | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/2[0-9]')
 
 if [[ -z "$network_config" ]]; then
-  # If network config is not OK, then get LAN information on 'eth0' and save to variable 'network_info', this subnet will be used in the NMAP command.
+  # If network config is not OK
+  # get LAN information on 'eth0' in CIDR notation
+  # save to variable 'network_info'.
   network_info=$(ip -o -f inet addr show | grep 'eth0' | awk '/scope global/ {print $4}')
 else
-  # If network config is OK, then use that subnet for NMAP command.
+  # If network config is OK
+  # use that subnet.
   network_info=$network_config
 fi
 
-# Aggresively scan LAN network for port TCP/9999
+# Aggressively scan LAN network for port TCP/9999
 # Grep for devices with port TCP/9999 open
 # Grep for IP addresses of devices with port TCP/9999 open
 echo "$network_info" > ../python/data/ip.txt
